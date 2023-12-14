5124f0899818240efbed19856ead0c26ad1abe58
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-15 00:32:34 +0100

    updated ui

--- react-game/src/components/ScoreModule.js
+++ react-game/src/components/ScoreModule.js
@@ -1,9 +1,13 @@
 import "../Game.css";
+import SnakePng from "../assets/images/snake-game-over.png"
 
 function ScoreModule(){
     return (
         <div className="score-board-container">
             <div className="score-board">
+            <div style={{display:"flex",justifyContent:"center"}}>
+            <img className="score-board-img" src={SnakePng} />
+            </div>
             <h1 className="score-board-text">Game Over!!!</h1>
             <h3 style={{textAlign:"Center"}}>Your Score:25</h3>
             
