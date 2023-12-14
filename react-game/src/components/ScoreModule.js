99d3cefa089231a03b6f56030d825c7d7a65a81d
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-15 00:07:57 +0100

    added animation for score board

--- react-game/src/components/ScoreModule.js
+++ react-game/src/components/ScoreModule.js
@@ -2,10 +2,13 @@ import "../Game.css";
 
 function ScoreModule(){
     return (
-        <div className="scoreBoardContainer">
-            <div className="scoreBoard">
-            <h1 className="scoreBoardText">Game Over!!!</h1>
+        <div className="score-board-container">
+            <div className="score-board">
+            <h1 className="score-board-text">Game Over!!!</h1>
+            <h3 style={{textAlign:"Center"}}>Your Score:25</h3>
+            
         </div>
+        
         </div>
     );
 }
