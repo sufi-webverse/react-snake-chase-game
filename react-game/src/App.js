281f2cb8e4bc92dd0f40636e92cc120c3bf9db6f
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-14 23:36:24 +0100

    created scoreBoard component

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
-import "./App.css";
+
 
 import BouncingBalls from "./components/BouncingBalls";
+
 function App() {
   return (
     <div className="">
       <div className="">
         <BouncingBalls />
+       
       </div>
     </div>
   );
---
+++ react-game/src/components/ScoreModule.js
@@ -0,0 +1,11 @@
+import "../Game.css";
+
+function ScoreModule(){
+    return (
+        <div className="scoreBoard">
+            Game Over
+        </div>
+    );
+}
+
+export default ScoreModule;
